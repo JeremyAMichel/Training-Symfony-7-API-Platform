7f330455ee9f6dd12f940a1a8ae5cc6cc8be1c67
JeremyAMichel <jeremy.a.michel@hotmail.fr>
2025-03-03 09:43:07 +0100

    demo

--- src/State/Provider/MeProvider.php
+++ src/State/Provider/MeProvider.php
@@ -1,5 +1,39 @@
 <?php
 
+// namespace App\State\Provider;
+
+// use ApiPlatform\Metadata\Operation;
+// use ApiPlatform\State\ProviderInterface;
+// use Symfony\Bundle\SecurityBundle\Security;
+// use Symfony\Component\HttpFoundation\RequestStack;
+
+// class MeProvider implements ProviderInterface
+// {
+//     public function __construct(
+//         private Security $security,
+//         private RequestStack $requestStack
+//     ) {
+//     }
+
+//     public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null
+//     {
+//         $request = $this->requestStack->getCurrentRequest();
+//         $authHeader = $request->headers->get('Authorization');
+        
+//         // Extraction du token (retire le "Bearer " du début)
+//         $token = str_replace('Bearer ', '', $authHeader);
+        
+//         // Affiche le token et l'utilisateur courant
+//         dd([
+//             'token' => $token,
+//             'user' => $this->security->getUser()
+//         ]);
+
+//         return $this->security->getUser();
+//     }
+// }
+
+
 namespace App\State\Provider;
 
 use ApiPlatform\Metadata\Operation;
@@ -15,6 +49,7 @@ class MeProvider implements ProviderInterface
 
     public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null
     {
+        
         return $this->security->getUser();
     }
 }
